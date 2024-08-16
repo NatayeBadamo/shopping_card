dcdfbd61eac1de61fa1dfe5bb4e49cb06d62ed95
lowin <149538744+NatayeBadamo@users.noreply.github.com>
2024-08-16 01:40:36 +0000

    added react-dom

--- shopping_card/src/main.jsx
+++ shopping_card/src/main.jsx
@@ -4,6 +4,7 @@ import './index.css';
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import RouteConfig from './routes/Routes.jsx';
 
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <RouteConfig />
